ac7359897d9fabd41798778acf8733fbcefc21a5
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-11-18 18:38:00 -0500

    more changes

--- data-json.js
+++ data-json.js
@@ -8,9 +8,24 @@ xhr.onload = function() {                       // When readystate changes
   console.log(response);
   console.log('response is type of', typeof response)
 
-  var elResponse = document.getElementById('divResponse')
-  elResponse.innerHTML = response;
-  
+    let jsonfile = '{ "name":"John", "age":30, "city":"New York"}'
+    var obj = JSON.parse(jsonfile);
+    console.log(obj)
+
+    var json = '{"result":true, "count":42}';
+    obj = JSON.parse(json);
+    console.log(obj.count);
+
+    const jsonplane = '{"Year": "2008","Month": "1","DayofMonth": "3","DayOfWeek": "4"}';
+    obj1 = JSON.parse(jsonplane);
+    console.log(obj1)
+
+    var eljsonPlane = document.getElementById('eljsonPlane')
+    eljsonPlane.innerHTML = obj1;
+
+    var elResponse = document.getElementById('divResponse')
+    elResponse.innerHTML = response;
+
   }
 };
 
