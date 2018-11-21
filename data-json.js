7d5116630d7931c58127bb4666a703242ab1d469
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-11-21 17:27:33 -0500

    m

--- data-json.js
+++ data-json.js
@@ -8,8 +8,6 @@ xhr.onload = function() {                       // When readystate changes
   console.log(response);
   console.log('response is type of', typeof response)
 
-
-
     const obj1 = {
       "Year": "2008",
       "Month": "1",
@@ -41,15 +39,108 @@ xhr.onload = function() {                       // When readystate changes
       "SecurityDelay": "NA",
       "LateAircraftDelay": "NA"};
 
+
+      const obj2 = [{
+        "Year": "2008",
+        "Month": "1",
+        "DayofMonth": "3",
+        "DayOfWeek": "4",
+        "DepTime": "932",
+        "CRSDepTime": "930",
+        "ArrTime": "1152",
+        "CRSArrTime": "1145",
+        "UniqueCarrier": "WN",
+        "FlightNum": "2361",
+        "TailNum": "N348SW",
+        "ActualElapsedTime": "140",
+        "CRSElapsedTime": "135",
+        "AirTime": "124",
+        "ArrDelay": "7",
+        "DepDelay": "2",
+        "Origin": "MCO",
+        "Dest": "PIT",
+        "Distance": "834",
+        "TaxiIn": "7",
+        "TaxiOut": "9",
+        "Cancelled": "0",
+        "CancellationCode": null,
+        "Diverted": "0",
+        "CarrierDelay": "NA",
+        "WeatherDelay": "NA",
+        "NASDelay": "NA",
+        "SecurityDelay": "NA",
+        "LateAircraftDelay": "No"},{
+        "Year": "2018",
+        "Month": "2",
+        "DayofMonth": "3",
+        "DayOfWeek": "4",
+        "DepTime": "932",
+        "CRSDepTime": "930",
+        "ArrTime": "11152",
+        "CRSArrTime": "1145",
+        "UniqueCarrier": "WN",
+        "FlightNum": "2361",
+        "TailNum": "N348SW",
+        "ActualElapsedTime": "140",
+        "CRSElapsedTime": "135",
+        "AirTime": "1214",
+        "ArrDelay": "7",
+        "DepDelay": "2",
+        "Origin": "MCO",
+        "Dest": "PIET",
+        "Distance": "834",
+        "TaxiIn": "7",
+        "TaxiOut": "9",
+        "Cancelled": "0",
+        "CancellationCode": null,
+        "Diverted": "0",
+        "CarrierDelay": "NA",
+        "WeatherDelay": "NA",
+        "NASDelay": "NA",
+        "SecurityDelay": "NA",
+        "LateAircraftDelay": "YES"},
+        {
+          "Year": "2038",
+          "Month": "3",
+          "DayofMonth": "3",
+          "DayOfWeek": "4",
+          "DepTime": "628",
+          "CRSDepTime": "620",
+          "ArrTime": "804",
+          "CRSArrTime": "750",
+          "UniqueCarrier": "WN",
+          "FlightNum": "448",
+          "TailNum": "N428WN",
+          "ActualElapsedTime": "96",
+          "CRSElapsedTime": "90",
+          "AirTime": "76",
+          "ArrDelay": "14",
+          "DepDelay": "8",
+          "Origin": "IND",
+          "Dest": "BWI",
+          "Distance": "515",
+          "TaxiIn": "3",
+          "TaxiOut": "17",
+          "Cancelled": "0",
+          "CancellationCode": null,
+          "Diverted": "0",
+          "CarrierDelay": "NA",
+          "WeatherDelay": "NA",
+          "NASDelay": "NA",
+          "SecurityDelay": "NA",
+          "LateAircraftDelay": "NA"
+        }
+      ];
+
     console.log('obj1 is ', obj1)
     console.log(typeof obj1)
+    console.log('obj2 is ', obj2)
+    console.log(typeof obj2)
 
     // iterating through the object of 'obj1', this works.
 
       for (let item in obj1) {
-
-          console.log(`The value is ${obj1[item]} of key  ${item}`);
-
+          console.log(`For obj1 the value is ${obj1[item]} of key  ${item}`);
           // var eljsonPlane = document.getElementById('eljsonPlane')
           // eljsonPlane.innerHTML = item;
 
@@ -63,6 +154,16 @@ xhr.onload = function() {                       // When readystate changes
             $("ol").text("flight found!!!");
           }
       }
+
+
+      Object.keys(obj2).forEach(key => {
+          console.log('the obj2[key]is', obj2[key]);   // the value of the current key.
+
+      for (let item in obj2) {
+          console.log(`For obj2 the value is ${obj2[item]} of key  ${item}`);
+        }
+
+      });
 
     var elResponse = document.getElementById('divResponse')
     elResponse.innerHTML = response;
