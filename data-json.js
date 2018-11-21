6a87b4374324aa9658536fba84ca45ca1a8a43dd
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-11-21 17:43:12 -0500

    m

--- data-json.js
+++ data-json.js
@@ -8,37 +8,6 @@ xhr.onload = function() {                       // When readystate changes
   console.log(response);
   console.log('response is type of', typeof response)
 
-    const obj1 = {
-      "Year": "2008",
-      "Month": "1",
-      "DayofMonth": "3",
-      "DayOfWeek": "4",
-      "DepTime": "932",
-      "CRSDepTime": "930",
-      "ArrTime": "1152",
-      "CRSArrTime": "1145",
-      "UniqueCarrier": "WN",
-      "FlightNum": "2361",
-      "TailNum": "N348SW",
-      "ActualElapsedTime": "140",
-      "CRSElapsedTime": "135",
-      "AirTime": "124",
-      "ArrDelay": "7",
-      "DepDelay": "2",
-      "Origin": "MCO",
-      "Dest": "PIT",
-      "Distance": "834",
-      "TaxiIn": "7",
-      "TaxiOut": "9",
-      "Cancelled": "0",
-      "CancellationCode": null,
-      "Diverted": "0",
-      "CarrierDelay": "NA",
-      "WeatherDelay": "NA",
-      "NASDelay": "NA",
-      "SecurityDelay": "NA",
-      "LateAircraftDelay": "NA"};
-
 
       const obj2 = [{
         "Year": "2008",
@@ -50,7 +19,7 @@ xhr.onload = function() {                       // When readystate changes
         "ArrTime": "1152",
         "CRSArrTime": "1145",
         "UniqueCarrier": "WN",
-        "FlightNum": "2361",
+        "FlightNum": "1361",
         "TailNum": "N348SW",
         "ActualElapsedTime": "140",
         "CRSElapsedTime": "135",
@@ -79,7 +48,7 @@ xhr.onload = function() {                       // When readystate changes
         "ArrTime": "11152",
         "CRSArrTime": "1145",
         "UniqueCarrier": "WN",
-        "FlightNum": "2361",
+        "FlightNum": "361",
         "TailNum": "N348SW",
         "ActualElapsedTime": "140",
         "CRSElapsedTime": "135",
@@ -109,7 +78,7 @@ xhr.onload = function() {                       // When readystate changes
           "ArrTime": "804",
           "CRSArrTime": "750",
           "UniqueCarrier": "WN",
-          "FlightNum": "448",
+          "FlightNum": "2361",
           "TailNum": "N428WN",
           "ActualElapsedTime": "96",
           "CRSElapsedTime": "90",
@@ -132,36 +101,41 @@ xhr.onload = function() {                       // When readystate changes
         }
       ];
 
-    console.log('obj1 is ', obj1)
-    console.log(typeof obj1)
     console.log('obj2 is ', obj2)
     console.log(typeof obj2)
 
     // iterating through the object of 'obj1', this works.
 
-      for (let item in obj1) {
-          console.log(`For obj1 the value is ${obj1[item]} of key  ${item}`);
-          // var eljsonPlane = document.getElementById('eljsonPlane')
-          // eljsonPlane.innerHTML = item;
+      // for (let item in obj1) {
+      //     console.log(`For obj1 the value is ${obj1[item]} of key  ${item}`);
+      //     // var eljsonPlane = document.getElementById('eljsonPlane')
+      //     // eljsonPlane.innerHTML = item;
+      //
 
-          $('div').after('<ol id="notice">ol:id=notice</p>');
-          $('ol#notice').before('output:',  item,' ',obj1[item],', ');
 
-          if (obj1[item]==2361) {
-            console.log('FOUND! flightnum is 2361')
-            $("ol").css("color", "red");
-            $("ol").css("font-size", "30px");
-            $("ol").text("flight found!!!");
-          }
-      }
 
 
       Object.keys(obj2).forEach(key => {
-          console.log('the obj2[key]is', obj2[key]);   // the value of the current key.
 
-      for (let item in obj2) {
-          console.log(`For obj2 the value is ${obj2[item]} of key  ${item}`);
-        }
+          console.log('the obj2[key]is', obj2[key]);
+
+          for (let item2 in obj2[key]) {
+              console.log(`Key is ${item2} and value is ${obj2[key][item2]}`);
+
+
+              $('div').after('<ol id="notice">ol:id=notice</p>');
+              $('ol#notice').before('output: ',  item2, ':' ,obj2[key][item2],' , ');
+
+              if (obj2[key][item2]==2361) {
+                console.log('OBJ2 FOUND! flightnum is 2361')
+                $("ol").css("color", "red");
+                $("ol").css("font-size", "30px");
+                $("ol").text("flight found!!!");
+              }
+
+          }
+
+
 
       });
 
